f6a7de0f8d072557634d8e0d24965b82bec84f10
Silvester Kresna W. P. P. <18221049@std.stei.itb.ac.id>
2024-06-09 22:55:32 +0700

    ubah API

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,57 @@
-import { useState, useEffect } from 'react'
-import Card from './component/Card'
-import News from './component/News'
+import { useState, useEffect } from "react";
+import Card from "./component/Card";
+import News from "./component/News";
 import { ref, onValue } from "firebase/database";
-import db from '../FirebaseConfig'
-import Dashboard from './component/Dashboard';
-import axios from 'axios';
+import db from "../FirebaseConfig";
+import Dashboard from "./component/Dashboard";
+import axios from "axios";
+import { newsData } from "../newsData";
 
 function App() {
   // const [count, setCount] = useState(0)
   const [temperature, setTemperature] = useState();
-  const [news,setNews] = useState([]);
+  const [news, setNews] = useState([]);
 
-  
   async function readDataTemperature() {
-    const databaseRef = ref(db,'test/temperature');
-    onValue(databaseRef,(snapshot)=>{
+    const databaseRef = ref(db, "test/temperature");
+    onValue(databaseRef, (snapshot) => {
       if (snapshot.exists()) {
         console.log(snapshot.val());
         setTemperature(snapshot.val());
       } else {
         setTemperature(0);
       }
-    })
-    
+    });
   }
 
-  async function getNews(){
-    const response = await axios.get('https://newsapi.org/v2/everything?q=fish&apiKey=a3e47262b4244628bca6522058c3a451');
+  async function getNews() {
+    // dibawah ini adalah
+    // const response = await axios.get('https://newsapi.org/v2/everything?q=fish&apiKey=a3e47262b4244628bca6522058c3a451');
+    const response = newsData;
     const berita = [];
-    const articles = response.data.articles;
-    berita.push(articles[7], articles[1], articles[2]);
+    // const articles = response.data.articles;
+    berita.push(response[7], response[1], response[2]);
     console.log(berita);
     setNews(berita);
     console.log(berita);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     readDataTemperature();
     getNews();
-  },[])
+  }, []);
 
   return (
     <div className="flex flex-col items-center lg:justify-between lg:flex-row lg:items-start p-20 gap-10">
-      <div className='flex flex-col gap-4 lg:w-[800px]'>
-        <p className='font-proxima text-[36px] font-bold'>Danang’s Aquarium</p>
-        <Card temperature={temperature}/>
-        <p className='font-proxima text-[36px] font-bold' >News</p>
-        <News berita = {news}/>
+      <div className="flex flex-col gap-4 lg:w-[800px]">
+        <p className="font-proxima text-[36px] font-bold">Danang’s Aquarium</p>
+        <Card temperature={temperature} />
+        <p className="font-proxima text-[36px] font-bold">News</p>
+        <News berita={news} />
       </div>
       <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
